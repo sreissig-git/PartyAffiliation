23f7854679a907c39b1425fca2f0c5bf5fa20556
sreissig-git <reissisn@mail.uc.edu>
2026-06-18 00:05:04 -0400

    Task completed

--- src/PartyAffiliation.java
+++ src/PartyAffiliation.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
+//Class PartyAffiliation
+//  main()
+//      String partyAffiliation
+//      output "Please enter your party affiliation: "
+//      input partyAffiliation
+//      if (partyAffiliation == "D")
+//          output "Donkey"
+//      else if (partyAffiliation == "R")
+//          output "Elephant"
+//      else if (partyAffiliation == "I")
+//          output "Person"
+//      else
+//          output "Other"
+//      endif
+//  return
+//EndClass
 public class PartyAffiliation {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String partyAffiliation;
+        System.out.println("Please enter your party affiliation (D, R, I, or other party): ");
+        partyAffiliation = in.nextLine();
+        if (partyAffiliation.equals("D")) {
+            System.out.println("Donkey");
+        } else if (partyAffiliation.equals("R")) {
+            System.out.println("Elephant");
+        } else if (partyAffiliation.equals("I")) {
+            System.out.println("Person");
+        } else {
+            System.out.println("Other");
+        }
+    }
 }
+
